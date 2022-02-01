210875266d9a6a23dddf090828c424dd0b503fdf
ramla199 <75176930+ramla199@users.noreply.github.com>
2022-02-01 19:20:49 +0000

    gggg

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 
+app.get("/", function (req, res) {
+    res.send("Hello World");
+})
 
 
 
@@ -36,4 +39,6 @@ var app = express();
 
 
 
- module.exports = app;
+
+
+module.exports = app;
