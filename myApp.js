193e16b7d854413119858402d54529a89ad279b7
ramla199 <75176930+ramla199@users.noreply.github.com>
2022-02-03 23:14:43 +0000

    gghu

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
-
-
+const path = require('path');
+const absolutePath = path.join(__dirname, '/views/index.html');
+app.use("/public", express.static(path.join(__dirname, "/public")));
 app.get("/", function (req, res) {
-    res.send(console.log("Hello World"));
+    res.sendFile(absolutePath);
+
 })
+
+
+
+
 
 
 
